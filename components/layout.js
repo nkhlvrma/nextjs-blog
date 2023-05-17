ff4f52a3096dd2a834fd39c46590b7f07742d450
nkhlvrma <nkhlvrma@gmail.com>
2023-05-17 11:34:58 +0530

    Update Layout.js

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Nikhil Kumar';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Next.js Blog Website';
 
 export default function Layout({ children, home }) {
   return (
